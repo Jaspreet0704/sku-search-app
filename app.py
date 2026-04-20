8c608b24093d3ef49c6a9cbbe4571d030751585b
Jaspreet0704 <kaurjass130704@gmail.com>
2026-04-20 17:25:05 +0530

    app.py

--- app.py
+++ app.py
@@ -10,45 +10,71 @@ def clear_search():
 
 # ---------------- DATA LOAD ----------------
 url = "https://docs.google.com/spreadsheets/d/1J9GQA2pg8jSl0ydN0CeFVVIJz7bRVP9VyOIAh2hv_Vs/export?format=csv&gid=42975298"
-
-# ✅ Correct CSV export link
 img_sheet_url = "https://docs.google.com/spreadsheets/d/1mBTa0m7gZ-57wbVf5kshc6hYNAT1o68HPzWsrlhd6cs/export?format=csv&gid=0"
+link_sheet_url = "https://docs.google.com/spreadsheets/d/1J9GQA2pg8jSl0ydN0CeFVVIJz7bRVP9VyOIAh2hv_Vs/export?format=csv&gid=565493070"
+link_df = pd.read_csv(link_sheet_url, header=1,on_bad_lines='skip')
+link_df.columns = link_df.columns.str.strip()
 
 df = pd.read_csv(url, header=1, on_bad_lines='skip')
 img_df = pd.read_csv(img_sheet_url)
 
-# ✅ Clean column names
 img_df.columns = img_df.columns.str.strip()
 
 df = df[[
     "Image link 1", "New Color SKU", "New SKU", "Parent SKU",
     "Color 1", "Color 2", "EAN CODE", "Box code",
     "Amazon SKU", "FK SKU", "AJIO SKU", "Myntra SKU",
-    "Meesho Product ID", "Amazon Child ASIN", "Amazon Parent ASIN"
+    "Meesho Catalog ID", "Amazon Child ASIN", "Amazon Parent ASIN"
 ]]
 
 # ---------------- SEARCH BAR ----------------
 if "search" not in st.session_state:
     st.session_state.search = ""
 
-col1, col2 = st.columns([0.5,1])
+col1, col2, col3 = st.columns([1,2,1])
 
 with col1:
-    st.text_input("Enter SKU", key="search")
+    search_column = st.selectbox(
+        "Search Field",
+        [
+            "New SKU",
+            "Parent SKU",
+            "New Color SKU",
+            "EAN CODE",
+            "Amazon SKU", "Amazon Child ASIN", "Amazon Parent ASIN",
+            "FK SKU",
+            "Myntra SKU",
+            "AJIO SKU",  
+            "Meesho Catalog ID"
+        ]
+    )
 
 with col2:
+    st.text_input("Enter Value", key="search")
+
+with col3:
     st.button("❌", on_click=clear_search)
 
 # ---------------- SEARCH ----------------
 if st.session_state.search:
     search = st.session_state.search.lower()
-
-    result = df[
-        df.apply(lambda col: col.astype(str).str.lower().str.contains(search, na=False)).any(axis=1)
-    ]
+    
+    result = df[df[search_column].astype(str).str.lower().str.contains(search, na=False)]
 
     if not result.empty:
         row = result.iloc[0]
+        color_sku = str(row["New Color SKU"]).strip()
+        link_row = link_df[link_df["New Color SKU"].astype(str).str.strip() == color_sku]
+        if not link_row.empty:
+            link_row = link_row.iloc[0]
+            
+            amazon_link = link_row.get("Amazon Link", "")
+            flipkart_link = link_row.get("Flipkart Link", "")
+            myntra_link = link_row.get("Myntra Link", "")
+            ajio_link = link_row.get("Ajio Link", "")
+            meesho_link = link_row.get("Meesho Link", "")
+        else:
+            amazon_link = flipkart_link = myntra_link = ajio_link = meesho_link = ""
 
         # ---------------- MAIN LAYOUT ----------------
         left_col, right_col = st.columns([1,2])
@@ -64,7 +90,7 @@ if st.session_state.search:
             if not img_row.empty:
                 img_url = str(img_row.iloc[0]["Original URL"]).strip()
 
-                # ✅ Dropbox FIX for iPhone
+                # Dropbox FIX for iPhone
                 if "dropbox.com" in img_url:
                     if "dl=1" in img_url:
                         img_url = img_url.replace("dl=1", "raw=1")
@@ -112,8 +138,11 @@ if st.session_state.search:
                 st.write("**Box code**")
                 st.write(row["Box code"])
             with col3:
-                st.write("**Amazon SKU**")
-                st.write(row["Amazon SKU"])
+                if amazon_link:
+                    st.markdown(f"**Amazon SKU**  [🔗]({amazon_link})")
+                    st.write(row["Amazon SKU"])
+                else:
+                    st.write("**Amazon SKU**", row["Amazon SKU"])
             with col4:
                 st.write("**Amazon Child ASIN**")
                 st.write(row["Amazon Child ASIN"])
@@ -123,19 +152,33 @@ if st.session_state.search:
 
             st.markdown("---")
 
-            col1, col2,col3 = st.columns(3)
+            col1, col2,col3, col4 = st.columns(4)
 
             with col1:
-                st.write("**Flipkart SKU**")
-                st.write(row["FK SKU"])
+                if amazon_link:
+                    st.markdown(f"**Flipkart SKU**  [🔗]({flipkart_link})")
+                    st.write(row["FK SKU"])
+                else:
+                    st.write("**Flipkart SKU**", row["FK SKU"])
             with col2: 
-                st.write("**Myntra**") 
-                st.write(row["Myntra SKU"])
+                if amazon_link:
+                    st.markdown(f"**Myntra SKU**  [🔗]({myntra_link})")
+                    st.write(row["Myntra SKU"])
+                else:
+                    st.write("**Myntra SKU**", row["Myntra SKU"])
             with col3: 
-                st.write("**Ajio**") 
-                st.write(row["AJIO SKU"])
+                if amazon_link:
+                    st.markdown(f"**AJIO SKU**  [🔗]({ajio_link})")
+                    st.write(row["AJIO SKU"])
+                else:
+                    st.write("**AJIO SKU**", row["AJIO SKU"])
+            with col4:
+                if amazon_link:
+                    st.markdown(f"**Meesho Catalog ID**  [🔗]({meesho_link})")
+                    st.write(str(row["Meesho Catalog ID"]).replace(".0",""))
+                else:
+                    st.write("**Meesho Catalog ID**", str(row["Meesho Catalog ID"]).replace(".0",""))
 
     else:
         st.error("No data found ❌")
-
 
